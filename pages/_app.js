b0ae33f2806f9ba89995eea51cc87d4f9b7caa29
kyrylo-holushko <kyrylo_holushko@hotmail.com>
2023-08-22 12:37:51 -0400

    useSWR for bags

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { useEffect } from "react";
 import { Container } from 'react-bootstrap';
 import RouteGuard from '../components/RouteGuard';
+import { getToken } from '../lib/authenticate';
 
 export default function App({ Component, pageProps }) {
     useEffect(()=>{
@@ -13,9 +14,26 @@ export default function App({ Component, pageProps }) {
     return (<>
         <RouteGuard>
             <Layout>
-                <Container>
-                    <Component {...pageProps} />
-                </Container>
+                <SWRConfig 
+                    value={{fetcher: async url => {
+                    const res = await fetch(url, {method: "GET",
+                    headers: {
+                        "authorization": `JWT ${getToken()}`,
+                        "content-type": "application/json"
+                    }});
+                    if (!res.ok) {
+                        const error = new Error('An error occurred while fetching the data.')
+                        error.info = await res.json()
+                        error.status = res.status
+                        throw error
+                    }
+                    return res.json()
+                    }
+                    }}>
+                    <Container>
+                        <Component {...pageProps} />
+                    </Container>
+                </SWRConfig>
             </Layout>
         </RouteGuard>
     </>)
--- pages/bags.js
+++ pages/bags.js
@@ -1,4 +1,17 @@
+import useSWR from 'swr';
+import { useEffect, useState } from "react"
+//const fetcher = (url) => fetch(url).then((res) => res.json()); 
+//import { getBags } from "../lib/ajax/bag";
+import { useSWRConfig } from 'swr'
+ 
+
+  
+ 
+
 export default function Bags(){
+    //const { data, error } = useSWR('getbags', fetcher);
+    const { fetcher } = useSWRConfig();
+    const { data, errors } = useSWR(`${process.env.NEXT_PUBLIC_LOCALHOST_API_URL}/bags`, fetcher);
 
     return (
         <>
