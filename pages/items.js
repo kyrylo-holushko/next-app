259c030dc867cc9149e9237b9397bddcf560d8dc
kyrylo-holushko <kyrylo_holushko@hotmail.com>
2023-08-25 12:17:51 -0400

    fixed bootstrap in items

--- pages/items.js
+++ pages/items.js
@@ -1,7 +1,7 @@
 import { BagContext } from "./_app";
 import { useContext, useEffect, useState } from "react";
 import { getItems } from "../lib/ajax/item";
-import { Container, Button, Table, Nav } from "react-bootstrap";
+import { Container, Button, Table, Row, Col } from "react-bootstrap";
 import ItemRow from "../components/item/ItemRow";
 import ItemCreate from "../components/item/itemCreate";
 
@@ -36,10 +36,12 @@ export default function Items(){
 
     return (
         <>
-            <Container className="p-5">
-                <h1>{bagName}</h1>
-                <Button variant="outline-secondary" onClick={handleShowItemCreate}>Add Item +</Button>
-                {errMsg && <h4 className="p-5">{errMsg}</h4>}
+            <Container className="px-5">
+                <Row>
+                    <Col sm md lg className="my-auto pt-4"><h2 className="d-inline">{bagName.toUpperCase()}</h2></Col>
+                    <Col md lg className="pt-4"><Button className="border-2" variant="outline-secondary" onClick={handleShowItemCreate} size="lg">Add Item +</Button></Col>
+                </Row>
+                {errMsg && <h4 className="pt-4">{errMsg}</h4>}
                 {itemData && <Table>
                     <thead>
                     <tr>
