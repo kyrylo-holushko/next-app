8bb1708ea9d7a4757d3d7b725d5600837532f68a
kyrylo-holushko <kyrylo_holushko@hotmail.com>
2023-08-30 11:16:38 -0400

    item details implemented

--- components/item/ItemDetails.js
+++ components/item/ItemDetails.js
@@ -1,12 +1,31 @@
-
-
-
+import { Modal, Card } from "react-bootstrap";
 
 export default function ItemDetails(props) {
 
+    const { iname, idesc, priority } = props.item;
+    const setShow = props.setShow;
+
+    const handleClose = () => {
+        setShow(false);
+    }
+
     return (
         <>
-
+            <Modal show={props.show} onHide={handleClose} centered>
+                <Modal.Header closeButton className="bg-light">
+                    <Modal.Title>{props.item?.iname}</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Card>
+                        <Card.Body>
+                            {props.item?.idesc}
+                        </Card.Body>
+                    </Card>
+                </Modal.Body>
+                <Modal.Footer>
+                    <h4>Priority: {props.item?.priority}</h4>
+                </Modal.Footer>
+            </Modal>
         </>
     )
 
