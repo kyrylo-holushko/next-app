c4240da3c1969762d1be74c4a417ad1933d6077a
kyrylo-holushko <kyrylo_holushko@hotmail.com>
2023-09-01 13:04:36 -0400

    added token removal on tab close

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { useEffect, useState, createContext } from "react";
 import { Container } from 'react-bootstrap';
 import RouteGuard from '../components/RouteGuard';
+import { removeToken } from '../lib/authenticate';
 
 export const BagContext = createContext();
 
@@ -12,7 +13,8 @@ export default function App({ Component, pageProps }) {
     const [bag, setBag] = useState(false);
 
     useEffect(()=>{
-      import("bootstrap/dist/js/bootstrap");
+        import("bootstrap/dist/js/bootstrap");
+        window.onbeforeunload = function(e){removeToken()};
     },[])
   
     return (<>
