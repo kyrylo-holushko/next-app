06e612498421fa8c8bed97c7ce74f0ab24ff13aa
kyrylo-holushko <kyrylo_holushko@hotmail.com>
2023-10-30 13:26:56 -0400

    Fixed bootstrap on navbar

--- components/Navigation.js
+++ components/Navigation.js
@@ -31,12 +31,14 @@ export default function MainNav() {
 
     return (
         <>
-            <Navbar inverse expand="sm" bg="secondary" data-bs-theme="dark">
+            <Navbar expand="sm" bg="secondary" data-bs-theme="dark">
                 <Container className="px-4">
+                    
                     {token && <Navbar.Brand className="align-self-start" href="#">{readToken().username.toUpperCase()}</Navbar.Brand>} 
-                    <div>
+                    
                     <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                     <Navbar.Collapse id="basic-navbar-nav">
+                    
                         <Nav className="me-auto">
                             {token && <Link href="/bags" legacyBehavior passHref><Nav.Link href="#bags">Bags</Nav.Link></Link>}
                         </Nav>
@@ -46,12 +48,13 @@ export default function MainNav() {
                                 <NavDropdown.Item onClick={(e)=>{logout()}}>Logout</NavDropdown.Item>
                             </NavDropdown>}                          
                         </Nav>
+                    
                     </Navbar.Collapse>
-                    </div>
-                    <div>
-                    {!token && <Button block variant="outline-light" onClick={handleShowLogin}>Login</Button>}
-                    {!token && <Button block variant="outline-light" className="ms-2" onClick={handleShowSignup}>Sign Up</Button>}
-                    </div>
+                    
+                    {!token && <div>
+                        <Button block variant="outline-light" onClick={handleShowLogin}>Login</Button>
+                        <Button block variant="outline-light" className="ms-2" onClick={handleShowSignup}>Sign Up</Button>
+                    </div>}
                 </Container>
             </Navbar>      
             <Login show={showLogin} setShow={setShowLogin}/>
